10d3e88a9bacc638f74f6cf0e890656e5ec44c9f
Pedro Haupt <pedro.haupt@parceirocantu.com.br>
2025-06-26 14:08:08 -0300

    Rebuildt

--- src/main/java/com/flash/editorimagem/controller/EdicaoController.java
+++
@@ -1,5 +0,0 @@
-package com.flash.editorimagem.controller;
-
-public class EdicaoController {
-    
-}
--- src/main/java/com/flash/editorimagem/model/imagens.java
+++ src/main/java/com/flash/editorimagem/model/imagens.java
@@ -6,7 +6,7 @@ public class imagens {
 
     private int id_imagem;
     private int usuario_id;
-    private String caminho_arquivo;
+    private String nome;
     private Date data_upload;
 
     public int getId_imagem() { return id_imagem; }
@@ -14,8 +14,8 @@ public class imagens {
     public int getUsuario_id() { return usuario_id; }
     public void setUsuario_id(int usuario_id) { this.usuario_id = usuario_id; }
     
-    public String getCaminho_arquivo() { return caminho_arquivo; }
-    public void setCaminho_arquivo(String caminho_arquivo) { this.caminho_arquivo = caminho_arquivo; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
     
     public Date getData_upload() { return data_upload; }
     public void setData_upload(Date data_upload) { this.data_upload = data_upload; }
